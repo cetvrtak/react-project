123f2f3a5b5cf13567a6055e24d3e564908f7e5d
cetvrtak <stevoiliskovic@gmail.com>
2023-07-21 13:51:43 +0200

    Render table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
 function App() {
+  const [rowData, setRowData] = useState();
+
+    const [columnDefs, setColumnDefs] = useState([
+        {field: 'symbol', filter: true},
+        {field: 'priceChange', filter: true},
+        {field: 'priceChangePercent'},
+        {field: 'weightedAvgPrice'},
+        {field: 'prevClosePrice'},
+        {field: 'lastPrice'},
+        {field: 'lastQty'},
+        {field: 'bidPrice'},
+        {field: 'bidQty'},
+        {field: 'askPrice'},
+        {field: 'askQty'},
+        {field: 'openPrice'},
+        {field: 'highPrice'},
+        {field: 'lowPrice'},
+        {field: 'volume'},
+        {field: 'quoteVolume'},
+        {field: 'openTime'},
+        {field: 'closeTime'},
+        {field: 'firstId'},
+        {field: 'lastId'},
+        {field: 'count'},
+    ]);
+
+  useEffect(() => {
+    fetch('https://data.binance.com/api/v3/ticker/24hr')
+        .then(result => result.json())
+        .then(rowData => setRowData(rowData))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ag-theme-alpine" style={{height: 500}}>
+
+        <AgGridReact
+            rowData={rowData} // Row Data for Rows
+            columnDefs={columnDefs} // Column Defs for Columns
+        />
+      </div>
     </div>
   );
 }
